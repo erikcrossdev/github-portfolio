5f0c2919b611e53024f41f90e7105a9a3aac27a5
ErikCruzEdify <erik.cruz@edifyeducation.com.br>
2025-02-14 16:22:52 -0300

    Add gallery elements

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ fetch("data.json") //when deploy, use
   })
   .then((data) => {
     const aboutMe = data.aboutMe;
+    const gallery = data.gallery; // Agora os dados são acessados corretamente
 
     // Atualizar a seção "about-me"
     const aboutMeSection = document.getElementById("about-me");
@@ -17,6 +18,26 @@ fetch("data.json") //when deploy, use
     aboutMeSection.querySelector("img").alt = `${aboutMe.name} Picture`;
     aboutMeSection.querySelector("h3").textContent = aboutMe.title;
     aboutMeSection.querySelector("p").textContent = aboutMe.description;
+
+    // Atualizar a seção "gallery"
+    const gallerySection = document.getElementById("gallery-content");
+
+    gallery.forEach((category) => {
+      const galleryElementDiv = document.createElement("div");
+      galleryElementDiv.classList.add("GalleryElement");
+
+      const projectTitle = document.createElement("h3");
+      projectTitle.textContent = category.projectName;
+      galleryElementDiv.appendChild(projectTitle);
+
+      const coverImage = document.createElement("img");
+      coverImage.src = category.coverImage;
+      coverImage.alt = `Capa de ${category.projectName}`;
+      // coverImage.classList.add("cover-image"); // Adiciona uma classe
+      galleryElementDiv.appendChild(coverImage);
+
+      gallerySection.appendChild(galleryElementDiv);
+    });
   })
   .catch((error) => {
     console.error("Erro ao carregar os dados:", error);
